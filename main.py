f148f922a410fbff33467d22670889be50baeea7
Jason Washburn <wburn@wburn.net>
2021-06-20 05:40:18 -0500

    builds wordclouds with stopwords

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from youtube_transcript_api import YouTubeTranscriptApi
+from wordcloud import WordCloud, STOPWORDS
 
 
 def create_string_from_transcript(transcript: list[dict]) -> str:
@@ -7,4 +8,17 @@ def create_string_from_transcript(transcript: list[dict]) -> str:
     return flat_text_string
 
 
+def build_wordcloud(text: str, addl_stop_words: list) -> WordCloud:
+    stopwords = set(STOPWORDS)
+    if addl_stop_words:
+        stopwords.update(addl_stop_words)
+
+    wordcloud = WordCloud(stopwords=stopwords).generate(text)
+    return wordcloud
+
 transcript = YouTubeTranscriptApi.get_transcript("i6bORQh_9LQ")
+
+flat_transcript = create_string_from_transcript(transcript=transcript)
+wordcloud = build_wordcloud(text=flat_transcript, addl_stop_words=["[Music]", "Music"])
+
+wordcloud.to_file("wordcloud.png")
